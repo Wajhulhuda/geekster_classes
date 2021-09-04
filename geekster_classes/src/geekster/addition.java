66f886877bdd0d8fb6313bda5fd6bf138418a9b1
Wajhul Huda <62687753+Wajhulhuda@users.noreply.github.com>
2021-09-04 10:40:04 +0530

    changes

---
+++ geekster_classes/src/geekster/addition.java
@@ -0,0 +1,13 @@
+package geekster;
+
+public class addition {
+	public static void main(String [] args) {
+		int num1 = 34;
+		int num2 = 43;
+		int num3 = 67;
+		int result;
+		result = num1 + num2 + num3;
+		System.out.println(result);
+	}
+
+}
---
+++ geekster_classes/src/geekster/min.java
@@ -0,0 +1,18 @@
+package geekster;
+
+public class min {
+	public static void main(String [] args) {
+		int num1 = 23;
+		int num2 = 34;
+		if (num1<num2) {
+			System.out.println(num1);
+		}
+		else {
+			System.out.println(num2);
+		}
+	}
+	
+		
+	
+
+}
